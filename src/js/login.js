fb1b7f719efeab5336f78202275cfd66dffc7a5c
MRichard97 <93174245+MRichard97@users.noreply.github.com>
2022-11-20 23:20:48 -0600

    COSAS

--- src/js/login.js
+++ src/js/login.js
@@ -3,13 +3,13 @@ let contraseña = document.getElementById("contraseña");
 let alerta = document.getElementById("alerta");
 let btnlogin = document.getElementById("login");
 
-alerta.style.display = 'none';
+alerta.style.display = "none";
 
 btnlogin.addEventListener('click', event => {
     event.preventDefault()
-    if (correo.value === 'notiunah@unah.hn' && contraseña.value === 'notiunah123') {
+    if (correo.value === 'notiunah@unah.hn' && contraseña.value === '123') {
       localStorage.setItem('isLoggedIn', 'true')
-      window.location.href = '/Panel'
+      window.location.href = '/panel/Panel'
     } else {
         alerta.style.display = 'block';
     }
